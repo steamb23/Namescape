8bcdd4f54674a30d7b09950fbd8a677f7a0089f1
SteamB23 <steamb23@gmail.com>
2014-02-28 00:35:36 +0900

    프로젝트폴더의 이름을 바꾸려고 뻘짓했다 ㅡㅅㅡ

---
+++ Namescape/Setting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Namescape
+{
+    class Setting
+    {
+        int charLength;
+        public Setting(decimal charLength)
+        {
+            this.charLength = (int)charLength;
+        }
+        public int CharLength
+        {
+            get
+            {
+                return charLength;
+            }
+        }
+    }
+}
