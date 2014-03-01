7f2f9d8bb8b039f40ba8367ca04ed1dcc3f6cdac
SteamB23 <steamb23@gmail.com>
2014-03-01 23:08:16 +0900

    한국이름 알고리즘 클래스 기초 생성

--- Namescape/Algorithm/Mod/AlgoKoreanName.cs
+++ Namescape/Algorithm/Mod/AlgoKoreanName.cs
@@ -7,6 +7,14 @@ namespace Namescape.Algorithm.Mod
 {
     class AlgoKoreanName : IAlgorithm
     {
+        /// <summary>
+        /// 한국어 인명용한자표에 나온 한자들의 발음만 조합합니다.
+        /// </summary>
+        /// <param name="setting"></param>
+        public AlgoKoreanName(Setting setting)
+        {
+            //개발전 메모: 두음 법칙도 적용해야함. 망할.
+        }
         string IAlgorithm.Result
         {
             get
@@ -14,5 +22,11 @@ namespace Namescape.Algorithm.Mod
                 throw new NotImplementedException();
             }
         }
+
+        //외부파일은 쓰기싫어서 string 배열로함.
+        public static string[] KoreanName = new string[]
+        {
+            //배열을 만들어 쓸거라서 나중을 기약하지.
+        };
     }
 }
