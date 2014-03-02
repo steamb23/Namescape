f8dca96d265e623d9872d47d1ab2ee809a041315
SteamB23 <steamb23@gmail.com>
2014-03-02 18:49:40 +0900

    테스트

---
+++ Namescape/Algorithm/Mod/Unicode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Namescape.Algorithm.Mod
+{
+    class Unicode : Algorithm
+    {
+        const int _UNICODE_RANGE_MIN = 0xac00;
+        const int _UNICODE_RANGE_MAX = 0xd7a3;
+        /// <summary>
+        /// 유니코드의 한글 범위를 조합합니다.
+        /// </summary>
+        /// <param name="setting">설정값을 받습니다.</param>
+        public Unicode(Setting setting)
+        {
+            for (int i = 0; i != setting.CharLength; i++)
+            {
+                result.Append((char)random.Next(_UNICODE_RANGE_MIN, _UNICODE_RANGE_MAX + 1));
+            }
+        }
+
+    }
+}
